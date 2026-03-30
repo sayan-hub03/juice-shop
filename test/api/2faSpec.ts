8a593e86acfaebc1f91adbd8820511b5ccd6f29d
Sayan Singh <115215536+sayan-hub03@users.noreply.github.com>
2026-03-30 23:50:15 +0530

    Fix formatting issues in 2faSpec.ts

--- test/api/2faSpec.ts
+++ test/api/2faSpec.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2014-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
-
+//test
 import frisby = require('frisby')
 import config from 'config'
 import jwt from 'jsonwebtoken'
